da8c2d859e5efe6f2b25a5f068e7651382dadc60
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-11-14 00:23:26 -0300

    get GameFindById controller, service

--- src/main/java/com/ventidue/dslist/controllers/GameController.java
+++ src/main/java/com/ventidue/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.ventidue.dslist.controllers;
 
+import com.ventidue.dslist.dto.GameDTO;
 import com.ventidue.dslist.dto.GameMinDTO;
 import com.ventidue.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 
 
--- src/main/java/com/ventidue/dslist/services/GameService.java
+++ src/main/java/com/ventidue/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.ventidue.dslist.services;
 
+
+import com.ventidue.dslist.dto.GameDTO;
 import com.ventidue.dslist.dto.GameMinDTO;
 import com.ventidue.dslist.entities.Game;
 import com.ventidue.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,8 +18,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(GameMinDTO::new).collect(Collectors.toList());
+    }
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).orElseThrow(() -> new RuntimeException("Game not found"));
+        return new GameDTO(result);
     }
 
 
